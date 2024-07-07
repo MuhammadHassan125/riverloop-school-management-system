e94ba0dc97f7a330e3e714e0b13b0e539942934f
MuhammadHassan125 <hassanalirajput2004@gmail.com>
2024-07-07 15:20:04 -0700

    change sidebar icons

--- src/components/SchoolDashboard/SchoolSidebarWrapper.jsx
+++ src/components/SchoolDashboard/SchoolSidebarWrapper.jsx
@@ -1,5 +1,9 @@
 import Sidebar, { SidebarItem } from '@/app/(admin-dashboard)/Sidebar'
 import React from 'react'
+import { PiChalkboardTeacher } from "react-icons/pi";
+import { RiHomeOfficeLine } from "react-icons/ri";
+import { FiUsers } from "react-icons/fi";
+import { IoSettingsOutline } from "react-icons/io5";
 import {
     LayoutDashboard,
     StickyNote,
@@ -37,22 +41,22 @@ const SchoolSidebarWrapper = ({isSidebarOpen}) => {
               link="/school-dashboard/calendar"
             />
             <SidebarItem
-              icon={<Layers size={20} />}
+              icon={<PiChalkboardTeacher size={20}/>}
               text="Teachers"
               link="/school-dashboard/teachers"
             />
             <SidebarItem
-              icon={<Layers size={20} />}
+              icon={<FiUsers  size={20} />}
               text="Users"
               link="/school-dashboard/users"
             />
             <SidebarItem
-              icon={<Layers size={20} />}
+              icon={<RiHomeOfficeLine  size={20} />}
               text="Office"
               link="/school-dashboard/office"
             />
             <SidebarItem
-              icon={<Layers size={20} />}
+              icon={<IoSettingsOutline  size={20} />}
               text="School Settings"
               link="#"
             />
